da1f57324c00ba5ae58481e61ee0c86d721eb497
joolzi <juliehorekens@gmail.com>
2016-05-18 15:47:24 -0400

    Fortuneteller done!

--- psets/10.js
+++ psets/10.js
@@ -9,4 +9,18 @@ Write a function named tellFortune that:
 - Call that function 3 times with 3 different values for the arguments.
 */
 
-// write your solution here...
+function tellFortune() {
+  var kids = ["3", "8", "4"];
+  var name = ["Felix", "Nathan", "Myles"];
+  var location = ["Paris", "Sukhothai", "Botswana"];
+  var job =  ["Programmer", "Uber Driver", "Painter"];
+
+  var randomkids = kids[Math.floor(Math.random()*kids.length)];
+  var randomname = name[Math.floor(Math.random()*name.length)];
+  var randomlocation = location[Math.floor(Math.random()*location.length)];
+  var randomjob = job[Math.floor(Math.random()*job.length)];
+
+  console.log("You will be a " +randomjob+ " in " +randomlocation+ ", and married to " +randomname+ " with " +randomkids+ " kids");
+}
+
+tellFortune();
